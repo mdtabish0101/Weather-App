a0c7f02873ddf4601b1d6699d46930dd56ae3e93
mdtabish0101 <tabishcoding0101@gmail.com>
2024-04-23 23:31:21 +0530

    API calling done

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+const button = document.getElementById('weather-button');
+const input = document.getElementById('city-input');
 
+async function getData(cityName){
+    const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=96db297f22984be4880163835242304&q=${cityName}&aqi=yes`
+    );
+    return await promise.json();
+}
+
+button.addEventListener('click', async () =>{
+    const value = input.value;
+    result = await getData(value);
+    console.log(result);
+});
 
