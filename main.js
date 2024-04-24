b0d4b3d48816d623d7ba35d438feb139a8874409
mdtabish0101 <tabishcoding0101@gmail.com>
2024-04-24 14:33:05 +0530

    api calling for getting current location

--- main.js
+++ main.js
@@ -36,3 +36,46 @@ button.addEventListener("click", async () => {
         weatherResult.innerHTML = "<p>Could not fetch weather data. Please try again.</p>"; // Display error message
     }
 });
+
+const locaButton = document.getElementById("location-button"); 
+const locationWeatherResult = document.getElementById("location-weather-result");
+
+
+async function getData2(lat, long) {
+    const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=96db297f22984be4880163835242304&q=${lat},${long}&aqi=yes`
+    );
+    if (response.ok) {
+        return await response.json(); // Return the JSON data
+    } else {
+        throw new Error("Failed to fetch weather data");
+    }
+}
+
+async function gotLocation(position){
+    const result = await getData2(position.coords.latitude, position.coords.longitude);
+    const current = result.current;
+    const location = result.location;
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const weatherHtml = `
+        <h2>Weather in ${location.name}, ${location.country}</h2>
+        <p>Latitude: ${latitude} , Longitude: ${longitude}</p>
+        <p>Temperature: ${current.temp_c}°C (${current.temp_f}°F)</p>
+        <p>Condition: ${current.condition.text}</p>
+        <img src="${current.condition.icon}" alt="Weather Icon"> <!-- Displaying weather icon -->
+    `;
+
+    locationWeatherResult.innerHTML = weatherHtml; // Insert the data into the HTML element
+}
+
+
+function errorLocation(){
+    locationWeatherResult.innerHTML = "<p>Could not get your location. Please check your device settings and try again.</p>";
+}
+
+locaButton.addEventListener("click", async () =>{
+    navigator.geolocation.getCurrentPosition(gotLocation,errorLocation);
+
+});
